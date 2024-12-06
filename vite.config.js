72b117a056902a95bcdd668873d0046d4ab77d8e
MozaSuryaPutra <mozasuryaputra@gmail.com>
2024-12-06 15:49:16 +0700

    benerin lagi

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from "vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+//import path from "path";
 
 export default defineConfig({
   plugins: [TanStackRouterVite(), react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
 });
