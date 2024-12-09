4b462bb7aa520329c9f135ff5c84f518a4873e05
MozaSuryaPutra <mozasuryaputra@gmail.com>
2024-12-10 00:54:45 +0700

    nyesuain vercel

--- src/routes/choose-return.lazy.jsx
+++ src/routes/choose-return.lazy.jsx
@@ -1,5 +1,5 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
-import ReturnFlight from "../components/ChooseFlight/returnFlight";
+import ReturnFlight from "../components/ChooseFlight/ReturnFlight";
 export const Route = createLazyFileRoute("/choose-return")({
   component: ChooseReturn,
 });
