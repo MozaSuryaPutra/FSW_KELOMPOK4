0ab6b26af0a3c81b709f221aa89f66fc713889e9
MozaSuryaPutra <mozasuryaputra@gmail.com>
2024-12-11 00:35:10 +0700

    search feature finish

--- src/components/ChooseFlight/ChooseFlight.jsx
+++ src/components/ChooseFlight/ChooseFlight.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+
 import {
   Container,
   Row,
@@ -8,7 +9,8 @@ import {
   Modal,
   ListGroup,
 } from "react-bootstrap";
-import { format } from "date-fns";
+
+import { addDays, format } from "date-fns";
 import { id } from "date-fns/locale";
 import { useNavigate, useLocation } from "@tanstack/react-router";
 import { useQuery } from "@tanstack/react-query";
@@ -251,10 +253,12 @@ function ChooseFlight() {
   const navigate = useNavigate();
   const { user, token } = useSelector((state) => state.auth);
   const [showModal, setShowModal] = useState(false);
+
   const [selectedSort, setSelectedSort] = useState("Harga - Termurah");
   const [sortedFlights, setSortedFlights] = useState(flightData);
-  const [selectedDay, setSelectedDay] = useState("2023-03-01");
+
   const [flightList, setFlight] = useState([]);
+  const [filteredFlights, setFilteredFlights] = useState([]);
   const location = useLocation();
   const {
     selectedDepartureCity,
@@ -267,6 +271,9 @@ function ChooseFlight() {
     passengersAmount,
     isReturnEnabled,
   } = location.state || {}; // Mendapatkan data dari state
+
+  console.log("ini selectetd departured date : ", selectedDepartureDate);
+  const [selectedDay, setSelectedDay] = useState(selectedDepartureDate);
   const dataToSend = {
     selectedDepartureCity,
     selectedReturnCity,
@@ -288,7 +295,7 @@ function ChooseFlight() {
       "flight",
       selectedDepartureCity,
       selectedReturnCity,
-      selectedDepartureDate,
+      selectedDay,
       selectedReturnDate,
       passengersAmount,
       selectedClass,
@@ -297,7 +304,7 @@ function ChooseFlight() {
       getSearchedFlight(
         selectedDepartureCity,
         selectedReturnCity,
-        selectedDepartureDate,
+        selectedDay,
         selectedReturnDate,
         passengersAmount, // Menggunakan modifiedPassengers yang sudah diproses
         selectedClass
@@ -319,7 +326,7 @@ function ChooseFlight() {
             data,
             selectedDepartureCity,
             selectedReturnCity,
-            selectedDepartureDate,
+            selectedDay,
             selectedReturnDate,
             selectedClass,
             selectedPassengers,
@@ -344,9 +351,65 @@ function ChooseFlight() {
     }
   }, [flightsData, isSuccess, error]);
 
+  useEffect(() => {
+    const filtered = flightList.departureFlights?.filter((flight) => {
+      const formattedDepartureDate = flight.departureDate.substring(0, 10);
+      return formattedDepartureDate === selectedDay;
+    });
+    setFilteredFlights(filtered);
+  }, [flightList, selectedDay]);
   if (isLoading) {
     return <p>Loading flights...</p>;
   }
+  // const filteredFlights = flightList.departureFlights?.filter((flight) => {
+  //   const formattedDepartureDate = flight.departureDate
+  //     .toString()
+  //     .substring(0, 10);
+  //   console.log(flight.departureDate.toString().substring(0, 10));
+  //   return formattedDepartureDate === selectedDay;
+  // });
+  const handleSortChange = (criteria) => {
+    setSelectedSort(criteria);
+
+    const sorted = [...filteredFlights]; // Salin array sebelum sorting
+    switch (criteria) {
+      case "Harga - Termurah":
+        sorted.sort((a, b) => a.price - b.price);
+        break;
+      case "Durasi - Terpendek":
+        sorted.sort((a, b) => {
+          const durationA = new Date(a.arrivalDate) - new Date(a.departureDate);
+          const durationB = new Date(b.arrivalDate) - new Date(b.departureDate);
+          return durationA - durationB;
+        });
+        break;
+      case "Keberangkatan - Paling Awal":
+        sorted.sort(
+          (a, b) => new Date(a.departureDate) - new Date(b.departureDate)
+        );
+        break;
+      case "Keberangkatan - Paling Akhir":
+        sorted.sort(
+          (a, b) => new Date(b.departureDate) - new Date(a.departureDate)
+        );
+        break;
+      case "Kedatangan - Paling Awal":
+        sorted.sort(
+          (a, b) => new Date(a.arrivalDate) - new Date(b.arrivalDate)
+        );
+        break;
+      case "Kedatangan - Paling Akhir":
+        sorted.sort(
+          (a, b) => new Date(b.arrivalDate) - new Date(a.arrivalDate)
+        );
+        break;
+      default:
+        break;
+    }
+
+    setFilteredFlights(sorted);
+    setShowModal(false);
+  };
 
   const onSubmit = async (event, flightId) => {
     event.preventDefault();
@@ -380,7 +443,7 @@ function ChooseFlight() {
         flightId,
         selectedDepartureCity,
         selectedReturnCity,
-        selectedDepartureDate,
+        selectedDay,
         selectedReturnDate,
         selectedClass,
         selectedPassengers,
@@ -394,11 +457,12 @@ function ChooseFlight() {
     return format(new Date(date), "d MMMM yyyy", { locale: id });
   };
 
-  //Filter flights based on the selected date
-  const filteredFlights = sortedFlights.filter(
-    (flight) => flight.date === selectedDay
-  );
-
+  // //Filter flights based on the selected date
+  // const filteredFlights = sortedFlights.filter(
+  //   (flight) => flight.date === selectedDay
+  // );
+  console.log("selectedfiltered : ", filteredFlights);
+  console.log("selectedday : ", selectedDay);
   return (
     <>
       <style>{`
@@ -450,48 +514,27 @@ function ChooseFlight() {
             md={12}
             className="text-center d-flex justify-content-center flex-wrap gap-1"
           >
-            {[
-              { day: "Selasa", date: "2023-03-01" },
-              { day: "Rabu", date: "2023-03-02" },
-              { day: "Kamis", date: "2023-03-03" },
-              { day: "Jumat", date: "2023-03-04" },
-              { day: "Sabtu", date: "2023-03-05" },
-              { day: "Minggu", date: "2023-03-06" },
-              { day: "Senin", date: "2023-03-07" },
-              { day: "Selasa", date: "2023-03-08" },
-            ].map((item, idx, array) => (
-              <div
-                key={idx}
-                className={`d-flex align-items-center ${
-                  idx < array.length - 1 ? "custom-border-end" : ""
-                } px-2`}
-              >
+            {[...Array(8)].map((_, idx) => {
+              const date = addDays(new Date(selectedDepartureDate), idx);
+              const formattedDate = format(date, "yyyy-MM-dd");
+              const dayName = format(date, "EEEE", { locale: id });
+
+              return (
                 <Button
+                  key={idx}
                   variant={
-                    selectedDay === item.date ? "primary" : "outline-secondary"
+                    selectedDay === formattedDate
+                      ? "primary"
+                      : "outline-secondary"
                   }
-                  onClick={() => setSelectedDay(item.date)}
+                  onClick={() => setSelectedDay(formattedDate)}
                   className="text-center"
                 >
-                  <span>{item.day}</span> <br />
-                  <small>{item.date}</small>
+                  <span>{dayName}</span> <br />
+                  <small>{formattedDate}</small>
                 </Button>
-              </div>
-            ))}
-            <style jsx>{`
-              .custom-border-end {
-                position: relative;
-              }
-
-              .custom-border-end::after {
-                content: "";
-                position: absolute;
-                right: 0;
-                top: 15%; /* Offset 15% dari atas */
-                height: 70%; /* Tinggi 70% dari kontainer */
-                border-right: 1px solid #ccc;
-              }
-            `}</style>
+              );
+            })}
           </Col>
         </Row>
 
@@ -516,11 +559,14 @@ function ChooseFlight() {
             </div>
 
             {/* Accordion for Flights */}
-
-            {flightList?.departureFlights?.length > 0 ? (
-              flightList.departureFlights.map((flight, idx) => (
-                <Accordion key={flight.id}>
-                  <Accordion.Item eventKey={idx} className="mb-3">
+            {filteredFlights?.length > 0 ? (
+              filteredFlights.map((flight, idx) => (
+                <Accordion
+                  key={flight.id}
+                  defaultActiveKey="0"
+                  className="mb-3"
+                >
+                  <Accordion.Item eventKey={idx}>
                     <Accordion.Header>
                       <div className="d-flex justify-content-between w-100 flex-wrap">
                         <div className="d-flex align-items-start gap-2">
@@ -649,7 +695,9 @@ function ChooseFlight() {
                         </Col>
                       </Row>
                       <Row>
-                        <p>{formatDate(flight.departureDate)}</p>
+                        <p>
+                          {flight.departureDate.toString().substring(0, 10)}
+                        </p>
                         <p>{flight.departureAirport.name}</p>
                         <hr style={{ width: "50%", margin: "0 auto" }}></hr>
                       </Row>
@@ -674,7 +722,7 @@ function ChooseFlight() {
                                   fontWeight: "bold",
                                 }}
                               >
-                                <li>Jet Air - {flightsData.class}</li>
+                                <li>Jet Air - {flight.class}</li>
                                 <li>{flight.airplane.airplaneCode}</li>
                               </ul>
                             </div>
@@ -727,8 +775,8 @@ function ChooseFlight() {
                         </Col>
                       </Row>
                       <Row>
-                        <p>{formatDate(flight.arrivalDate)}</p>
-                        <p>{flight.destinationAirport.id}</p>
+                        <p>{flight.arrivalDate.toString().substring(0, 10)}</p>
+                        {/* <p>{flight.destinationAirport.id}</p> */}
                       </Row>
                     </Accordion.Body>
                   </Accordion.Item>
@@ -757,8 +805,8 @@ function ChooseFlight() {
                   label: "Paling Akhir",
                   value: "Keberangkatan - Paling Akhir",
                 },
-                { label: "Paling Awal", value: "Kedatangan - Paling Awal" },
-                { label: "Paling Akhir", value: "Kedatangan - Paling Akhir" },
+                { label: "K. Awal", value: "Kedatangan - Paling Awal" },
+                { label: "K. Akhir", value: "Kedatangan - Paling Akhir" },
               ].map((sortOption, idx) => (
                 <ListGroup.Item
                   key={idx}
@@ -770,7 +818,7 @@ function ChooseFlight() {
                       : "d-flex justify-content-between align-items-center"
                   }
                 >
-                  {sortOption.value} {/* Menampilkan label yang lebih pendek */}
+                  {sortOption.label}
                   {selectedSort === sortOption.value && (
                     <span className="text-success ms-2">✔</span>
                   )}
